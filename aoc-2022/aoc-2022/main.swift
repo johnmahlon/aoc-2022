06571eac6540332d23d93c44a0f9e1fce57c9368
John Peden <john.peden@protonmail.com>
2022-12-02 15:58:04 -0600

    d2p1

--- aoc-2022/aoc-2022/main.swift
+++ aoc-2022/aoc-2022/main.swift
@@ -22,10 +22,74 @@ func processData() -> [Int] {
 let calorieSums = processData()
 
 // part 1
+print("Day 1:")
 print(calorieSums.last ?? -1)
 
 // part 2
 print(calorieSums[(calorieSums.count - 3)...].reduce(0, +))
+print("---------------------------------------")
+
+
+// MARK: - Day 2
+let scoreGuide = [
+    "R": 1,
+    "P": 2,
+    "S": 3,
+]
+
+let encryptGuide = [
+    "X": "R",
+    "Y": "P",
+    "Z": "S",
+    "A": "R",
+    "B": "P",
+    "C": "S"
+]
+
+func play(them: String, you: String) -> Int {
+    // if you tie
+    if them == you {
+        return 3
+    }
+    
+    // rock/scissor combo to handle wrapping
+    if them == "R" && you == "S" {
+        return 0
+    }
+    
+    if them == "S" && you == "R" {
+        return 6
+    }
+    
+    //otherwise, use score guide to compare
+    // also force unwrap because this is aoc, not prod
+    if scoreGuide[you]! > scoreGuide [them]! {
+        return 6
+    }
+    
+    // all other options, you lost
+    return 0
+}
+
+func process() -> Int {
+    read(filename: "day2.txt")
+        .components(separatedBy: .newlines)
+        .map {
+            $0.components(separatedBy: .whitespaces)
+                .map {
+                    encryptGuide[$0]!
+                }
+        }
+        .map {
+            play(them: $0[0], you: $0[1]) + scoreGuide[$0[1]]!
+        }
+        .reduce(0, +)
+        
+}
+
+print("Day 2:")
+print(process())
+
 
 
 // MARK: - Helpers
